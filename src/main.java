931e01988f1f75c356adafd94c088fc41818fc31
AKrelov <wsup@bk.ru>
2022-11-13 14:38:18 +0300

    initial commit

---
+++ src/main.java
@@ -0,0 +1,19 @@
+public class main {
+    public static void main(String[] args) {
+
+        int eaters = 5; // скольько людей будут есть
+
+        int woter = 3000; //милилитров воды
+        int potatoes = 5; //картофелин
+        int chiken = 6; //куриных бедер
+        int spices = 10;  // ложек специй
+
+        System.out.println("Сварили суп. На одного человека вышло.");
+        System.out.println((woter/eaters) + "милилитров воды");
+        System.out.println((potatoes/eaters) + "кортофелин(а)");
+        System.out.println((chiken/eaters) + "куриных(ое) бедер(ро)");
+        System.out.println((spices/eaters) + "ложек(ка) специй");
+
+    }
+
+}
